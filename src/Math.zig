856c8671d2925e1b14d9cd6ee1afa7c666a20d4d
GeffDev <88239718+GeffDev@users.noreply.github.com>
2022-11-13 10:56:43 +0800

    HOLY SHIT FINALLY

--- src/Math.zig
+++ src/Math.zig
@@ -14,7 +14,6 @@ pub var arcTan256LookupTable: [256 * 256]u8 = rsdk.std.mem.zeroes([256 * 256]u8)
 pub fn CalculateTrigAngles() void {
     var i: usize = 0;
 
-    // Do we need to increment the index at the end of the loop or at the start?
     while (i < 512) : (i += 1) {
         sinMLookupTable[i] = @floatToInt(i32, @sin(((@intToFloat(f32, i) / 256.0) * rsdk.std.math.pi) * 4096.0));
         cosMLookupTable[i] = @floatToInt(i32, @cos(((@intToFloat(f32, i) / 256.0) * rsdk.std.math.pi) * 4096.0));
@@ -47,22 +46,22 @@ pub fn CalculateTrigAngles() void {
     sin512LookupTable[384] = -512;
 
     i = 0;
-    while (i < 256) : (i += 1) {
+    while (i < 256) {
         // Do we need to multiply the index by 2?
         sin256LookupTable[i] = sin512LookupTable[i * 2] >> 1;
         cos256LookupTable[i] = cos512LookupTable[i * 2] >> 1;
+        i += 1;
     }
 
     var y: usize = 0;
-    while (y < 256) : (i += 1) {
+    while (y < 256) : (y += 1) {
         var atan: *u8 = &arcTan256LookupTable[y];
         var x: usize = 0;
         while (x < 256) : (x += 1) {
             var angle: f32 = rsdk.std.math.atan2(f32, @intToFloat(f32, y), @intToFloat(f32, x));
             atan.* = @floatToInt(u8, angle * 40.743664);
-            // 256 doesn't fit into u8?
-            // Nevermind, ducky sent a message about it, its about incrementing the pointer
-            atan.*[@intCast(usize, atan.*[] + 256)];
+            // terrifying pointer arithmetic tbh
+            atan = @intToPtr(*u8, @ptrToInt(atan) + 0x100);
         }
     }
 }
