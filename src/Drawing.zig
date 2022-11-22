7d5a40d3ff74a7108d9a64f9d83e28a76298c420
Elias Wright <88239718+GeffDev@users.noreply.github.com>
2022-11-22 13:20:03 +0800

    casting in zig kinda sucks tbhbbhhbhbh

--- src/Drawing.zig
+++ src/Drawing.zig
@@ -5,9 +5,9 @@ pub var subtractLookupTable: [256 * 32]u16 = rsdk.std.mem.zeroes([256 * 32]u16);
 pub var tintLookupTable: [65536]u16 = rsdk.std.mem.zeroes([65536]u16);
 
 pub fn generateBlendLookupTable() void {
-    var y: usize = 0;
+    var y: u16 = 0;
     while (y < 256) {
-        var x: usize = 0;
+        var x: u16 = 0;
         while (x < 32) {
             blendLookupTable[x + (32 * y)] = y * x >> 8;
             subtractLookupTable[x + (32 * y)] = y * (31 - x) >> 8;
@@ -16,10 +16,10 @@ pub fn generateBlendLookupTable() void {
         y += 1;
     }
 
-    var i: usize = 0;
+    var i: i32 = 0;
     while (i < 65536) {
         var tintValue: i32 = (i & 31) + ((i & 2016) >> 6) + ((i & 63488) >> 11);
-        tintLookupTable[i] = 2113 * rsdk.std.math.min(tintValue, 31);
+        tintLookupTable[@intCast(usize, i)] = @intCast(u16, 2113 * rsdk.std.math.min(tintValue, 31));
         i += 1;
     }
 }
