462edd3c9e81747d13d31cae0f32596a540818d6
Elias Wright <88239718+GeffDev@users.noreply.github.com>
2022-11-29 13:15:29 +0800

    kid named iniParser:

--- src/Ini.zig
+++ src/Ini.zig
@@ -8,16 +8,46 @@ pub const ItemType = enum {
     Comment,
 };
 
-pub const ItemFields = struct {
+pub const ItemConfig = struct {
     section: []u8,
-    hasSection: bool,
     key: []u8,
     value: []u8,
     type: ItemType,
 };
 
-pub var iniItems = rsdk.std.ArrayList(ItemFields).init(rsdk.allocator.allocator);
+pub var iniItems = rsdk.std.ArrayList(ItemConfig).init(rsdk.allocator.allocator);
 
 pub fn iniParser(fileName: []const u8) !void {
-    _ = fileName;
+    var buf: [256]u8 = rsdk.std.mem.zeroes([256]u8);
+    var section: [0x40]u8 = rsdk.std.mem.zeroes([0x40]u8);
+
+    var file = rsdk.std.fs.cwd().openFile(fileName, .{}) catch {
+        rsdk.std.log.err("couldn't open ini file {s}!", .{fileName});
+        return;
+    };
+    defer file.close();
+
+    var bufReader = rsdk.std.io.bufferedReader(file.Reader);
+    var inStream = bufReader.reader();
+
+    while (try inStream.readUntilDelimiterOrEof(&buf, '\n')) |line| : (buf = rsdk.std.mem.zeroes([256]u8)) {
+        const lineTrimmed = rsdk.std.mem.trim(u8, line, " \n");
+        var item: ItemConfig = rsdk.std.mem.zeroes(ItemConfig);
+
+        if (lineTrimmed[0] == '#') {
+            break;
+        }
+        if (line.len == 0) {
+            continue;
+        }
+
+        if (rsdk.std.mem.startsWith(u8, lineTrimmed, '[') and rsdk.std.mem.endsWith(u8, lineTrimmed, ']')) {
+            rsdk.std.mem.copy(u8, item.section, lineTrimmed[1..lineTrimmed.len - 1]);
+        
+            section = rsdk.std.mem.zeroes([64]u8);
+            rsdk.std.mem.copy(u8, &section, lineTrimmed[1..lineTrimmed.len - 1]);
+        } else if (rsdk.std.mem.indexOfScalar(u8, lineTrimmed, '=')) |index| {
+            _ = index;
+        }
+    }
 }
